3baa3aa72382a6f236d18a03b335945493fa3c83
Maser12q <rude.03@mail.ru>
2019-11-16 19:02:21 +0700

    New
    Signed-off-by: Maser12q <rude.03@mail.ru>

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print("My first git-repo")
+import pygame
+
+pygame.init()
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+
+pygame.draw.line(screen, (255, 255, 255), (0, 600), (800, 0), 5)
+pygame.draw.line(screen, (255, 255, 255), (0, 0), (800, 600), 5)
+pygame.display.flip()
+# ожидание закрытия окна:
+while pygame.event.wait().type != pygame.QUIT:
+    pass
