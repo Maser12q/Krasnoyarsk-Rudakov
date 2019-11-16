057b791980ff2800fa2e58b9e29ead5a60ae68a8
Maser12q <rude.03@mail.ru>
2019-11-16 19:18:24 +0700

    New
    Signed-off-by: Maser12q <rude.03@mail.ru>

--- first.py
+++ first.py
@@ -1,12 +1,12 @@
 import pygame
 
 pygame.init()
-size = width, height = 800, 600
+size = width, height = 800, 600  # tuple(map(int, input().split()))
 screen = pygame.display.set_mode(size)
 
-pygame.draw.line(screen, (255, 255, 255), (0, 600), (800, 0), 5)
-pygame.draw.line(screen, (255, 255, 255), (0, 0), (800, 600), 5)
+pygame.draw.rect(screen, (255, 0, 0), (1, 1, width - 2, height - 2))
 pygame.display.flip()
+
 # ожидание закрытия окна:
 while pygame.event.wait().type != pygame.QUIT:
     pass
