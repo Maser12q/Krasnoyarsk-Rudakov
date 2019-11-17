8f4123f768673e41e8654425f7697fbd589221c5
Сережа <rude.03@mail.ru>
2019-11-17 13:00:13 +0700

    home

--- first.py
+++ first.py
@@ -18,6 +18,10 @@ screen = pygame.display.set_mode(size)
 for i in range(k, 0, -1):
     us1, us2, us3 = (k - q) % 3 == 1, (k - q) % 3 == 2, (k - q) % 3 == 0
     pygame.draw.circle(screen, (255 * us3, 255 * us1, 255 * us2), (n * k, n * k), (n * i))
+    pygame.draw.circle(screen, (255 * us3, 255 * us1, 255 * us2), (0, n * k), (n * i))
+    pygame.draw.circle(screen, (255 * us3, 255 * us1, 255 * us2), (n * k, 0), (n * i))
+    pygame.draw.circle(screen, (255 * us3, 255 * us1, 255 * us2), (n * k * 2, n * k), (n * i))
+    pygame.draw.circle(screen, (255 * us3, 255 * us1, 255 * us2), (n * k, n * k * 2), (n * i))
     if q + 1 > 3:
         q = 1
     else:
