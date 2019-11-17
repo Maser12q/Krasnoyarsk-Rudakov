654a38c3aa106a88ae0ea73b58403e27a1fcfbf9
Сережа <rude.03@mail.ru>
2019-11-17 12:38:23 +0700

    home

--- first.py
+++ first.py
@@ -1,18 +1,28 @@
+"""С клавиатуры вводится толщина кольца N (в пикселях) и количество колец K.
+Нарисуйте «RGB мишень» из внутреннего круга и K колец чистого
+красного, зеленого, синего, красного и т.д. («по кругу») цветов максимальной яркости.
+Радиус маленького красного круга и толщина каждого кольца должны быть равны N пикселей.
+Мишень должна быть прижата к левому верхнему краю окна."""
+
 import pygame
 
-width, n = tuple(map(int, input().split()))
+"""место для объявления переменных"""
+q = 1
 
-pron = n % 2 != 1
-kol_vo = width // n
-size = width, width
+n, k = map(int, input().split())
+size = 2 * n * k, 2 * n * k
 
+# Начало отрисовки окна
 pygame.init()
 screen = pygame.display.set_mode(size)
+for i in range(k, 0, -1):
+    us1, us2, us3 = (k - q) % 3 == 1, (k - q) % 3 == 2, (k - q) % 3 == 0
+    pygame.draw.circle(screen, (255 * us3, 255 * us1, 255 * us2), (n * k, n * k), (n * i))
+    if q + 1 > 3:
+        q = 1
+    else:
+        q += 1
 
-for i in range(0, width + kol_vo, kol_vo):
-    for j in range(0, width, kol_vo):
-        if (j + i) % (2 * kol_vo) != 0:
-            pygame.draw.rect(screen, (255, 255, 255), (i - (kol_vo * pron), j, kol_vo, kol_vo))
 pygame.display.flip()
 
 while pygame.event.wait().type != pygame.QUIT:
