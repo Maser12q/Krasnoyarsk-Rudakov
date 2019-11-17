53a24a3fb573a29c90166779e842a259954e9abf
Сережа <rude.03@mail.ru>
2019-11-17 11:39:50 +0700

    home

--- first.py
+++ first.py
@@ -1,20 +1,21 @@
 import pygame
 
 pygame.init()
-width, n = tuple(map(int, input().split()))
+width, n = 900, 4  # tuple(map(int, input().split()))
 height = width
 size = width, height
 screen = pygame.display.set_mode(size)
 
+kol_vo = width // n
 for i in range(n):
     for j in range(n):
-        if (j + i) % (2 * n) != 0:
-            pygame.draw.rect(screen, (0, 0, 0), (i, j, n, n))
-            # print(i, j)
-        else:
-            pygame.draw.rect(screen, (255, 255, 255), (i, j, n, n))
-
-            # print('*' * 40)
+        if (j + i) % 2 != 0:
+            pygame.draw.rect(screen, (255, 255, 255), (i * kol_vo, j * kol_vo, kol_vo, kol_vo))
+            print(i, j)
+        # else:
+        #     pygame.draw.rect(screen, (0, 0, 0), (i + kol_vo, j + kol_vo, kol_vo, kol_vo))
+        #
+        #     print(i, j)
 
 pygame.display.flip()
 
