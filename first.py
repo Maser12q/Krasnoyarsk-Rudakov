a04df6bea6e698e5e068e14fadd8fd766edebf64
Сережа <rude.03@mail.ru>
2019-11-23 14:26:33 +0700

    home_v2

--- first.py
+++ first.py
@@ -1,77 +1,51 @@
 import pygame
-from pygame.math import Vector2
 
-"""классы объектов, без них не работает"""
-
-
-class Object:
-    def __init__(self):
-        self.fps = 50
-        self.x_size = width
-        self.y_size = height
-        self.color = (255, 255, 255)
-        self.vi = 50
-        self.r = 70
-
-
-class Threeangle(Object):
-    def __init__(self, a1, a2):
-        super().__init__()
-        self.angle_1 = a1
-        self.angle_2 = a2
-        self.vt = 0
-        self.a1 = Vector2((101, self.r))
-
-        self.a2 = Vector2((101, self.r))
-
-        print(self.vt)
-
-    def low_v(self):
-        self.vt -= self.vi
-
-    def high_v(self):
-        self.vt += self.vi
-
-    def edit_angle(self):
-        self.angle_1 += int(self.vt / self.fps) * 3
-        self.angle_2 += int(self.vt / self.fps) * 3
-        self.a1.from_polar((self.r, self.angle_1))
-        self.a2.from_polar((self.r, self.angle_2))
-
-    def change_koord(self):
-        self.edit_angle()
-        pygame.draw.polygon(screen, (255, 255, 255), [(101, 101), self.a1 + (101, 101), self.a2 + (101, 101)])
-        # print(self.vt)
-
-
-"""для настройки окна, типа void Start в Unity или Setup в arduino"""
-pygame.init()
-size = width, height = (201, 201)
-screen = pygame.display.set_mode(size)
 running = True
-clock = pygame.time.Clock()
-n = [Threeangle(255, 285), Threeangle(15, 45), Threeangle(165, 135)]
 
-"""главный цикл, типа void Update, хотя не, fixed_update, в unity"""
+
+# # классы объектов, без них не работает
+# class Object:
+#     def __init__(self):
+#         self.fps = 50
+#         self.x_size = width
+#         self.y_size = height
+#         self.color = (255, 255, 255)
+#         self.vi = 50
+#         self.r = 70
+
+def k_ku(x, y):
+    if y1 < y:
+        y -= 1
+    elif y1 > y:
+        y += 1
+
+    if x1 < x:
+        x -= 1
+    elif x1 > x:
+        x += 1
+    pygame.draw.circle(screen, (255, 0, 0), (x, y), 20)
+
+    return x, y
+
+
+# для настройки окна, типа void Start в Unity или Setup в arduino
+pygame.init()
+size = width, height = (501, 501)
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+x, y = 251, 251
+x1, y1 = 251, 251
+print(1)
+# главный цикл, типа void Update, хотя не, fixed_update, в unity
 while running:
     screen.fill((0, 0, 0))
-    pygame.draw.circle(screen, (255, 255, 255), (101, 101), 10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                # down
-                for i in n:
-                    i.low_v()
-            elif event.button == 3:
-                # up
-                for i in n:
-                    i.high_v()
+            x1, y1 = event.pos
 
-    for i in n:
-        i.change_koord()
-        # print(i.vt)
-
+    x, y = k_ku(x, y)
     clock.tick(50)
     pygame.display.flip()
