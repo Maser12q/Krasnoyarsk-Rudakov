516d4f8be8f1c769a2b6518de67924bab943344d
Maser12q <rude.03@mail.ru>
2019-12-04 20:18:57 +0700

    yadnex

--- first.py
+++ first.py
@@ -1,81 +1,51 @@
+import os
 import random
 
 import pygame
 
-# width, height =
-size = 1000, 1000
-fps = 60
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+
+size = width, height = 400, 300
 screen = pygame.display.set_mode(size)
-running = True
+
 clock = pygame.time.Clock()
 
+all_sprites = pygame.sprite.Group()
 
-def get_col():
-    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+bomb_image = load_image("bomb.png")
 
+for i in range(50):
+    bomb = pygame.sprite.Sprite(all_sprites)
+    bomb.image = bomb_image
+    bomb.rect = bomb.image.get_rect()
 
-class Board:
-    # создание поля
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = dict()  # (cell_x, cell_y): [cell_id, cell_val]
-        # значения по умолчанию
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
+    bomb.rect.x = random.randrange(width)
+    bomb.rect.y = random.randrange(height)
 
-    # настройка внешнего вида
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
-
-    def render(self, surf):
-        for i in range(self.left, self.left + self.width * self.cell_size, self.cell_size):
-            for j in range(self.top, self.top + self.height * self.cell_size, self.cell_size):
-                if (i, j) not in self.board.keys():
-                    self.board[(i, j)] = [(i // self.cell_size, j // self.cell_size), 0]
-                if self.board[(i, j)][1] != 1:
-                    pygame.draw.rect(surf, (255, 255, 255), (i, j, self.cell_size, self.cell_size), 1)
-                else:
-                    pygame.draw.rect(surf, (255, 255, 255), (i, j, self.cell_size, self.cell_size))
-
-    def get_cell(self, mouse_pos):
-        xm, ym = mouse_pos
-        for xc, yc in self.board.keys():
-            if xc <= xm <= xc + self.cell_size and yc <= ym <= yc + self.cell_size:
-                return xc, yc
-
-    def on_click(self, cell_coords):
-        if cell_coords is not None:
-            x, y = cell_coords
-            if self.board[(x, y)][1] == 1:
-                self.board[(x, y)][1] = 0
-            else:
-                self.board[(x, y)][1] = 1
-
-    def get_click(self, mouse_pos):
-        self.on_click(self.get_cell(mouse_pos))
-
-
-board = Board(10, 10)
-
-board.set_view(0, 0, 50)
-
+running = True
 while running:
-    screen.fill((0, 0, 0))
-    board.render(screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            board.get_click(event.pos)
-        if event.type == pygame.KEYDOWN:
-            board.board.clear()
-    screen.fill((0, 0, 0))
-    board.render(screen)
-    clock.tick(fps)
+    screen.fill(pygame.Color("white"))
+    all_sprites.draw(screen)
     pygame.display.flip()
 
-print(board.board)
+pygame.quit()
