d9cd35833bc9af4c30db2fc671b1afb50665e365
Сережа <rude.03@mail.ru>
2019-12-05 20:15:27 +0700

    for lev

--- test.py
+++ test.py
@@ -2,10 +2,15 @@ import requests
 
 
 def save_image_with_url(url):
+    out = open("img.jpg", "wb")  # где img.jpg твоя картинка
     try:
         img = requests.get(url)  # адрес картинки
+        out = open("img.jpg", "wb")  # где img.jpg твоя картинка
+        out.write(img.content)
     except ValueError:
         print('String dummles!')
-    out = open("img.jpg", "wb")  # где img.jpg твоя картинка
-    out.write(img.content)
-    out.close()
+    finally:
+        out.close()
+
+
+save_image_with_url('https://sun9-61.userapi.com/c850424/v850424769/14c42a/TClmxFEvnKY.jpg')
--- vk.py
+++ vk.py
@@ -1,42 +1,50 @@
-# import random
-#
-# import vk
-#
-# session = vk.Session('d40c66d6f609a4a50b4e470b6a0f015a1d69e982f3cc16addbb946d44dfa90eba540bf2e6d20f07b25ed8')
-# api = vk.API(session, v='5.103', lang='ru', timeout=10)
-#
-# api.messages.getLastActivity(user_id='248667120')
-# # print(api.messages.send())
-#
-# '''4d36712eec3fab80a5987e8efcb3824f1688aedeaadc65236e74b940e9008161b4ac06457aedf9e9464e4'''
 import random
+import sqlite3
 import time
 
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 
-# class QuestOne:
-#     def __init__(self):
-#         self.zapros = 0
-#         self.peer_id = -1
-#         self.chat_id = -1
-#         self.msg = {1: 'у тебя есть лям, что сделаешь', 2: 'ок, что будет дальше?',
-#                     3: 'Все ложь, и как итог: люби, страдай и какай, либо еби, но это опционально'}
-#
-#     def msg_def(self, vk, chat_id):
-#         self.zapros += 1
-#         vk.messages.send(chat_id=chat_id,
-#                          random_id=random.randint(1, 9489979897999),
-#                          message=self.msg.get(self.zapros, "квест закончен, техобслуживание"))
-#
+def quest(id_user):
+    db = sqlite3.connect('db_local.db')
+    try:
+        k = db.execute('SELECT chis FROM bot_info WHERE user_id = ?', id_user).fetchone()
+    except sqlite3.OperationalError:
+        db.execute('''CREATE TABLE bot_info (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id, chis INTEGER)''')
+        k = list()
+
+    if k == list():
+        db.execute('''''')
 
 
-def main():
+def listen_in_chat():
+    s = input()
+    if s.lower() == 'жека':
+        msg_for_chat(3)
+    elif s.lower() == 'тест':
+        msg_for_chat(1)
+    elif s.lower() in ['лиза', 'гсг', 'тест2']:
+        msg_for_chat(2)
     zap()
 
 
+def msg_for_chat(chat):
+    vk_session = vk_api.VkApi(
+        token='d40c66d6f609a4a50b4e470b6a0f015a1d69e982f3cc16addbb946d44dfa90eba540bf2e6d20f07b25ed8')
+    vk = vk_session.get_api()
+    vk.messages.send(chat_id=chat,
+                     random_id=random.randint(1, 9489979897999),
+                     message='Ку, инопланетяне! Давайте говорить\n\n\nНу гооо')
+
+
+def main():
+    quest(533609675)
+    listen_in_chat()
+
+
 def zap():
+    print('слушаю')
     msg = ' '
     mas = []
     vk_session = vk_api.VkApi(
@@ -49,7 +57,7 @@ def zap():
             print(event.obj)
             if len(str(event.obj.get('message').get('peer_id'))) > 9:
                 if event.obj.get('message').get('text').lower() in ['ммм', 'хентай', 'аниме', 'жопа', 'грудь']:
-                    msg = 'Хентай - топ!'
+                    msg = 'Хентай'
 
                 elif event.obj.get('message').get('text').lower() in ['...', 'заткнись', 'сука', 'пизда', 'хех']:
                     msg = '*ушёл*'
@@ -58,13 +66,12 @@ def zap():
                                      message=msg)
                     time.sleep(2)
                     msg = '*пришёл*'
-                #
-                # elif 'квест' in event.obj.get('message').get('text').lower():
-                #
-                #     if event.obj.get('message').get('from_id') not in mas:
-                #         s = (event.obj.get('message').get('peer_id') - 2000000000)
-                #         r.msg_def(vk, s)
-
+                elif 'актив' in event.obj.get('message').get(
+                        'text').lower() and '[club189131918|@my_gk]' in event.obj.get('message').get('text').lower():
+                    for _ in range(10):
+                        vk.messages.send(chat_id=event.obj.get('message').get('peer_id') - 2000000000,
+                                         random_id=random.randint(1, 9489979897999),
+                                         message='общение')
                 else:
                     msg = ''
 
@@ -77,7 +84,7 @@ def zap():
 
             else:
                 if event.obj.get('message').get('text').lower() in ['ммм', 'хентай', 'аниме', 'жопа', 'грудь']:
-                    msg = 'Хентай - топ!'
+                    msg = 'Хентай'
                 elif event.obj.get('message').get('text').lower() in ['...', 'заткнись', 'сука', 'пизда', 'хех']:
                     msg = '*ушёл*'
                     vk.messages.send(chat_id=event.obj.get('message').get('peer_id') - 2000000000,
@@ -94,21 +101,6 @@ def zap():
                                      message=msg)
                 except vk_api.exceptions.ApiError:
                     pass
-
-
-        elif event.type == VkBotEventType.MESSAGE_REPLY:
-            print('Новое сообщение:')
-            print('От меня для: ', end='')
-            print(event.obj.peer_id)
-            print('Текст:', event.obj.text)
-            print()
-
-        elif event.type == VkBotEventType.MESSAGE_TYPING_STATE:
-            print('Печатает ', end='')
-            print(event.obj.from_id, end=' ')
-            print('для ', end='')
-            print(event.obj.to_id)
-            print()
 
 
 if __name__ == '__main__':
