1b9d38210841d349c2e36c15ba8e88c4dfb41bb5
Сережа <rude.03@mail.ru>
2019-12-17 23:24:31 +0700

    Растояние между городами)))

--- test.py
+++ test.py
@@ -29,3 +29,35 @@
 #
 #     os.system('shutdown -s -t 0')
 
+
+# -*- coding: utf-8 -*-
+# import pyodbc
+#
+# conn = pyodbc.connect(
+#     r'Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=D:\Games\Krasnoyarsk-Rudakov\sp_manuals.mdb;')
+#
+# cursor = conn.cursor()
+# cursor.execute('select * from DOC')
+#
+# for row in cursor.fetchall():
+#     import re
+#
+#     s = str(row[3]).encode('cp-1251')
+#     print(re.sub(r'\\x..', '', s))
+#
+#
+import geopy
+from geopy.distance import geodesic
+geolocator = geopy.Nominatim(user_agent="specify_your_app_name_here")
+
+g1_name = input('Введите адрес первой точки, типа: Улица, дом, город')
+g2_name = input('Введите адрес второй точки, типа: Улица, дом, город')
+
+g1_cod = geolocator.geocode(g1_name)
+g2_cod = geolocator.geocode(g2_name)
+
+g1_loc = (g1_cod.latitude, g1_cod.longitude)
+g2_loc = (g2_cod.latitude, g2_cod.longitude)
+
+print('Одну секунду, растояние сейчас появится')
+print(f'{round(geodesic(g1_loc, g2_loc).kilometers, 0)} километров')
